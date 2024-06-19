81d5ffa68540bc07199cfcccb8be4b2b80535a18
Gilbert Ondonga Nyamberi <hunchovault@gmail.com>
2024-06-19 06:41:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  var member = {
+    name: 'John Doe',
+    age: 25,
+    skills: ['JavaScript', 'React', 'Node'],
+    details: function() {
+      this.skills.forEach(function(skill) {
+        console.log(`${this.name} knows ${skill}`);
+      });
+    }
+  };
+
+  member.details();
+}
